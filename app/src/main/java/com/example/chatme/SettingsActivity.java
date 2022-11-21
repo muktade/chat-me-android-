e984ba7b3c7dedee300abee83c5ada09642b151a
muktadeer <muktadeer@gmail.com>
2022-11-21 13:45:20 +0600

    setting pic ses p...

--- app/src/main/java/com/example/chatme/SettingsActivity.java
+++ app/src/main/java/com/example/chatme/SettingsActivity.java
@@ -1,22 +1,40 @@
 package com.example.chatme;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.chatme.databinding.ActivitySettingsBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class SettingsActivity extends AppCompatActivity {
 
     ActivitySettingsBinding binding;
+    FirebaseDatabase database;
+    FirebaseAuth auth;
+    FirebaseStorage storage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivitySettingsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         getSupportActionBar().hide();
+
+        ////
+        database = FirebaseDatabase.getInstance();
+        auth= FirebaseAuth.getInstance();
+        storage = FirebaseStorage.getInstance();
 
         binding.btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,5 +43,34 @@ public class SettingsActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        binding.btnAddImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/**");
+                startActivityForResult(intent,33);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(data.getData()!= null){
+            Uri sFile = data.getData();
+            binding.profileImage.setImageURI(sFile);
+
+            final StorageReference reference = storage.getReference()
+                    .child("profile_pictures")
+                    .child(auth.getUid());
+            reference.putFile(sFile).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Toast.makeText(SettingsActivity.this, "Upload Success", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
 }
